7770ad2a7a5857a7e957bcc18373bea6533f0ad5
Ali Şentürk <ali.senturk@gmail.com>
2018-03-25 23:43:58 +0300

    on yüz tasarım

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { NutritionCalc } from './nutrition-calc'
 
 @Component({
   selector: 'page-about',
@@ -7,8 +8,10 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
+  model:NutritionCalc = new NutritionCalc();
+  
   constructor(public navCtrl: NavController) {
-
+    this.model.cinsiyet = 'e';
   }
 
 }
---
+++ src/pages/about/nutrition-calc.ts
@@ -0,0 +1,14 @@
+export class NutritionCalc{
+    public  boy:number;
+    public  kilo:number;
+    public  yas:number;
+    public  cinsiyet:string;
+    public  stresDegeri:number;
+    public  aktiviteDegeri:number;
+
+    public  hrb:number;
+    public  bazalEnerji:number;
+    public  idealKilo:number;
+    public  gunlukEnerji:number;
+        
+}
